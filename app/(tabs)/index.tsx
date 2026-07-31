b16be413f382a7df5d055bab2506aeebcccdb382
david05-Bonilla <bonilla.coca2027@hilasal.superate.org.sv>
2026-07-30 22:36:58 -0600

    Actualizar archivos existentes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,67 @@
-import React, { useState } from 'react'; 
-import { Platform, StyleSheet, TextInput, Button } from 'react-native'; 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 
 export default function HomeScreen() {
   const [valorInput, setValorInput] = useState('');
 
   return (
     <ThemedView style={styles.container}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" >Log in </ThemedText>
-        
-      </ThemedView>
+      <View style={StyleSheet.absoluteFill} pointerEvents="none">
+        <Svg height="2700" width="150%" viewBox=" -5 -55 60 1050" preserveAspectRatio="none">
+          <Path
+            d="M -1,-100 C 0,350 200,110 -40,510 Z"
+            fill="#00162F"
+            opacity="1"
+          />
+        </Svg>
+      </View>
+      <View style={styles.titleContainer}>
+        <ThemedText style={styles.titleText}>Log in </ThemedText>
 
-      <ThemedView style={styles.stepContainer}>
+      </View>
+
+      <View style={styles.stepContainer}>
         <ThemedText type="subtitle">Correo electrónico</ThemedText>
         <TextInput
           style={styles.input}
-          placeholder="tu correo @email.com"
+          placeholder="       tu correo @email.com"
           placeholderTextColor="#888"
           value={valorInput}
           onChangeText={setValorInput}
           keyboardType="email-address"
           autoCapitalize="none"
         />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+      </View>
+
+      <View style={styles.stepContainer}>
         <ThemedText type="subtitle">Contraseña</ThemedText>
         <TextInput
           style={styles.input}
-          placeholder="********"
+          placeholder="       ********"
           placeholderTextColor="#888"
           value={valorInput}
           onChangeText={setValorInput}
           keyboardType="email-address"
           autoCapitalize="none"
         />
-      </ThemedView>
-      <ThemedView style={styles.buttom}>
-        <Button 
-          title="Log in" 
+      </View>
+      <Link href="/modal" style={styles.link}>
+        <ThemedText>¿Olvidaste tu contraseña?</ThemedText>
+      </Link>
+      <View style={styles.buttom}>
+        <Button
+          title="Log in"
           color="#004695"
-          onPress={() => alert('¡Botón presionado!')} 
         />
-      </ThemedView>
+      </View>
+      <ThemedText type="subtitle" style={styles.text}>¿No tienes cuenta?</ThemedText>
+      <Link href="/modal" style={styles.link2}>
+        <ThemedText type='link'>Registrate</ThemedText>
+      </Link>
     </ThemedView>
   );
 }
@@ -53,37 +71,69 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     justifyContent: 'center',
-    marginTop: -200,
-  
+    marginBottom: -170,
+    
   },
   titleContainer: {
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 8,
-    marginBottom: 50,
+    marginBottom: 30,
+    height: 90,
+  },
+  titleText: {
+    fontSize: 70, 
+    fontWeight: 'bold',
+    height: 80,
+    paddingBottom: -200,
+    paddingTop: 20,
   },
   stepContainer: {
     gap: 8,
     marginBottom: 16,
     
   },
- 
+
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
-    borderRadius: 8,
+    borderRadius: 50,
     padding: 12,
     fontSize: 16,
     backgroundColor: '#fff',
     color: '#000',
   },
-  buttom:{
+  buttom: {
     borderWidth: 10,
     borderColor: '#004695',
     backgroundColor: '#004695',
     borderRadius: 100,
-    fontSize: 16,
-    marginLeft: 100,
-    marginRight: 100,
-  }
+    fontSize: 20,
+    marginLeft: 70,
+    marginRight: 70,
+    marginTop: 80,
+    paddingVertical: 10,
+    overflow: 'hidden',
+  },
+  link: {
+    marginTop: 5,
+    paddingVertical: 15,
+    alignSelf: 'center',
+    color: '#fff', 
+    fontWeight: 'bold',
+  },
+  link2: {
+    marginTop: 5,
+    paddingVertical: 15,
+    alignSelf: 'center',
+    color: '#fff', 
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 20,
+  },
 });
--- constants/theme.ts
+++ constants/theme.ts
@@ -19,7 +19,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#00162F',
+    background: '#004695',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
